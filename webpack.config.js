ce0da12a67956d2f720fb0f2d4fa5b177a1f820c
Grace <huang.yahui.grace@gmail.com>
2022-02-27 11:51:46 +0800

    add css and js hash to src directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
       path: path.resolve(__dirname, 'dist'),
-      filename: 'main.bundle.js'
+      filename: 'main.[hash].bundle.js'
     },
     devServer: {
         static: {
@@ -18,8 +20,13 @@ module.exports = {
         rules: [
             { 
                 test: /\.css$/i, 
-                use: ['style-loader', 'css-loader'], 
+                use: [MiniCssExtractPlugin.loader, 'css-loader'], 
             }
         ]
-    }
+    },
+    plugins: [new HtmlWebpackPlugin({
+            template: './base.html'
+        }),
+        new MiniCssExtractPlugin()
+    ],
   };
